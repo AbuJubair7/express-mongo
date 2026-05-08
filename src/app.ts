edbfcad5aa5eb8d045187b19b9d5d8e5a15b35bf
AbuJubair7 <jubaircode7@gmail.com>
2026-05-08 10:13:01 +0600

    Mongo clusted created and checked

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,22 @@
-import hi from "./hi.js";
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
-console.log("Hello, World!");
-console.log(hi("Jubair"));
+const app = express();
+
+app.use(express.json());
+
+const uri = process.env.MONGO_URI as string;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Successfully connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log("Database connection failed:", error);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
