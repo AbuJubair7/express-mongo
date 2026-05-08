a979281f6b34489adacab27dc1d063a1d43980a4
AbuJubair7 <jubaircode7@gmail.com>
2026-05-08 09:49:07 +0600

    Setup nodemon and solved configuration error

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,4 @@
+import hi from "./hi.js";
+
 console.log("Hello, World!");
+console.log(hi("Jubair"));
---
+++ src/hi.ts
@@ -0,0 +1,3 @@
+export default function hi(name: string): string {
+  return `Hellow there, ${name}!`;
+}
